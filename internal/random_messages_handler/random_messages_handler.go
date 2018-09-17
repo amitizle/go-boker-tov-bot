46a3e3106231fabbb542a2ef605e8920bf090025
Amit Goldberg <amit.goldberg@gmail.com>
2018-09-17 21:08:01 +0100

    Remove some sentences from random handler

--- internal/random_messages_handler/random_messages_handler.go
+++ internal/random_messages_handler/random_messages_handler.go
@@ -1,25 +1,22 @@
 package random_messages_handler
 
 import (
-	"fmt"
 	"gopkg.in/telegram-bot-api.v4"
 	"math/rand"
 	"time"
 )
 
 var (
-	replyProbability = 0.17
+	replyProbability = 0.08
 	replies          = []string{
-		"מה הדיבור @%s?",
-		"מה נז׳מע @%s",
-		"איפה תלך @%s",
-		"הכל רמייה @%s",
-		"@%s שב בלול",
-		"@%s על הצוואר שרשרת מגן דוד ומלשינים דוקר תמיד",
-		"@%s אדידס לובש בנות כובש",
-		"הרגת אותנו @%s",
-		"מהנרגילה שואף ואת כל המלשינים מכפכף, לא ככה @%s?",
-		"הרגת אותנו @%s",
+		"מה נז׳מע",
+		"מנשמע",
+		"וואאההה",
+		"פףף",
+		"טחח",
+		"איפה תלך",
+		"הכל רמייה",
+		"ניסים צ׳מע",
 	}
 )
 
@@ -27,7 +24,7 @@ func Callback() func(*tgbotapi.BotAPI, *tgbotapi.Message) {
 	return func(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
 		if rand.Float64() <= replyProbability {
 			response := replies[rand.Intn(len(replies))]
-			msg := tgbotapi.NewMessage(message.Chat.ID, fmt.Sprintf(response, message.From))
+			msg := tgbotapi.NewMessage(message.Chat.ID, response)
 			bot.Send(msg)
 		}
 	}
